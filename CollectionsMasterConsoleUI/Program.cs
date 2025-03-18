ee68052660fd044380cb7861d1d5f428529d6892
Shannon Ashford <email.sa.coder@gmail.com>
2025-03-18 01:14:43 -0400

    Updated formulas

--- CollectionsMasterConsoleUI/Program.cs
+++ CollectionsMasterConsoleUI/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace CollectionsMasterConsoleUI
 {
@@ -7,48 +11,103 @@ namespace CollectionsMasterConsoleUI
     {
         static void Main(string[] args)
         {
-            //TODO: Follow the steps provided in the comments under each region.
+            // 1] TODO: Follow the steps provided in the comments under each region.
             //Make the console formatted to display each section well
             //Utlilize the method stubs at the bottom for the methods you must create ⬇⬇⬇
 
             #region Arrays
-            //TODO: Create an integer Array of size 50
-            
+            // 2] TODO: Create an integer Array of size 50
+            int[] myArray = new int[50];
 
-            //TODO: Create a method to populate the number array with 50 random numbers that are between 0 and 50
-            
+            // 3] TODO: Create a method to populate the number array with 50 random numbers that are between 0 and 50
+            ///////////////////////////////////////////////
+            ///
+            // See Methods section below
 
-            //TODO: Print the first number of the array
+            Populater(myArray);
+            //var randomArray = new Random();
+            //Random rng = new Random();
 
-            //TODO: Print the last number of the array            
+            ////myArray = rand.Next(50);
+
+            ////myArray{ Populater();};
+
+            //for (int i = 0;  i < myArray.Length; i++)
+            //{
+            //    myArray[i] = rng.Next(0,51);
+                
+            //    //random(0, myArray.Length);
+            //    //Populater(int[] myArray);
+            //}
+            //Console.WriteLine(myArray);
+
+            //myArray.Print();
+
+            /////////////////////////////////////////////////
+            // 4] TODO: Print the first number of the array
+            Console.WriteLine(myArray[0]);
+
+            // 5] TODO: Print the last number of the array            
+            Console.WriteLine(myArray[myArray.Length-1]);
+            Console.WriteLine(myArray[49]);
+            Console.WriteLine();
 
             Console.WriteLine("All Numbers Original");
-            //UNCOMMENT this method to print out your numbers from arrays or lists
-            //NumberPrinter();
+            // 6] UNCOMMENT this method to print out your numbers from arrays or lists
+
+            NumberPrinter(myArray);
             Console.WriteLine("-------------------");
 
-            //TODO: Reverse the contents of the array and then print the array out to the console.
+            // 7] TODO: Reverse the contents of the array and then print the array out to the console.
             //Try for 2 different ways
             /*  1) First way, using a custom method => Hint: Array._____(); 
                 2) Second way, Create a custom method (scroll to bottom of page to find ⬇⬇⬇)
             */
 
+            Array.Reverse(myArray);
+
+            Console.WriteLine(myArray);
+
             Console.WriteLine("All Numbers Reversed:");
 
             Console.WriteLine("---------REVERSE CUSTOM------------");
 
+            ReverseArray(myArray);
+
             Console.WriteLine("-------------------");
 
-            //TODO: Create a method that will set numbers that are a multiple of 3 to zero then print to the console all numbers
+            // 8] TODO: Create a method that will set numbers that are a multiple of 3 to zero then print to the console all numbers
             Console.WriteLine("Multiple of three = 0: ");
-            
+
+            //for (i = 0; i < myArray.Length; i++)
+            //{
+            //if (i % 3 == 0)
+            //{
+            //i = 0;
+            //}
+            //else
+            //{
+            //i = i;
+
+            ThreeKiller(myArray);
+
+                                
+            //}
+            //Console.WriteLine(byThrees());
+
+            //Console.WriteLine(myArray);
+
 
             Console.WriteLine("-------------------");
 
-            //TODO: Sort the array in order now
+            // 9] TODO: Sort the array in order now
             /*      Hint: Array.____()      */
-            Console.WriteLine("Sorted numbers:");
-            
+
+
+            Console.WriteLine("******Sorted Numbers:*******");
+            Array.Sort(myArray);
+            NumberPrinter(myArray);
+            Console.WriteLine();
 
             Console.WriteLine("\n************End Arrays*************** \n");
             #endregion
@@ -57,69 +116,148 @@ namespace CollectionsMasterConsoleUI
             Console.WriteLine("************Start Lists**************");
 
             /*   Set Up   */
-            //TODO: Create an integer List
-            
+            // 10 ] TODO: Create an integer List
+            var create = new List<int>();
 
-            //TODO: Print the capacity of the list to the console
-            
+            // 11] TODO: Print the capacity of the list to the console
+            Console.WriteLine(create.Capacity);
 
-            //TODO: Populate the List with 50 random numbers between 0 and 50 you will need a method for this            
-            
+            // 12] TODO: Populate the List with 50 random numbers between 0 and 50 you will need a method for this            
+            Populater(create);
 
-            //TODO: Print the new capacity
-            
+            // 13] TODO: Print the new capacity
+            Console.WriteLine(create.Capacity);
 
             Console.WriteLine("---------------------");
-
-            //TODO: Create a method that prints if a user number is present in the list
+            ///////
+            // 14] TODO: Create a method that prints if a user number is present in the list
             //Remember: What if the user types "abc" accident your app should handle that!
             Console.WriteLine("What number will you search for in the number list?");
-            
-            Console.WriteLine("-------------------");
+            bool meetCriteria;
+            int searchNumber;
 
+            do
+            {
+                Console.WriteLine("Enter a number:");
+                meetCriteria = int.TryParse(Console.ReadLine(), out searchNumber);
+
+            } while (!meetCriteria);
+
+            NumberChecker(create, searchNumber);
+           
+            Console.WriteLine("-------------------");
+            /////////
             Console.WriteLine("All Numbers:");
-            //UNCOMMENT this method to print out your numbers from arrays or lists
-            //NumberPrinter();
+            // 15] UNCOMMENT this method to print out your numbers from arrays or lists
+            NumberPrinter(create);
             Console.WriteLine("-------------------");
 
 
-            //TODO: Create a method that will remove all odd numbers from the list then print results
-            Console.WriteLine("Evens Only!!");
+            // 16] TODO: Create a method that will remove all odd numbers from the list then print results
             
+            Console.WriteLine("Evens Only!!");
+
+            OddKiller(create);
+
             Console.WriteLine("------------------");
 
-            //TODO: Sort the list then print results
+
+            // 17] TODO: Sort the list then print results
             Console.WriteLine("Sorted Evens!!");
             
+            create.Sort();
+            NumberPrinter(create);
+
             Console.WriteLine("------------------");
 
-            //TODO: Convert the list to an array and store that into a variable
-            
+            // 18] TODO: Convert the list to an array and store that into a variable
 
-            //TODO: Clear the list
-            
+            int[] newCreateArray = create.ToArray();
+
+            // 19] TODO: Clear the list
+
+            create.Clear();
+
+           
 
             #endregion
         }
 
+        #region
+        private static void byThrees()
+        {
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    i = 0;
+                }
+                else
+                {
+                    i = i;
+
+                }
+            }
+        }
+
+        //private static void removeOdds()
+        //{
+            
+        //    create.RemoveAt(i);
+            
+                
+        //}
+
         private static void ThreeKiller(int[] numbers)
         {
-            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 3 == 0)
+                {
+                    numbers[i] = 0;
+                }
+                //else
+                //{
+                    //numbers[i] = numbers[i];
+                //}
+            }
+            NumberPrinter(numbers);
         }
 
-        private static void OddKiller(List<int> numberList)
+        private static void OddKiller(List<int> create)
         {
-            
+            for (int i = create.Count - 1; i >= 0; i--)
+            {
+                if (create[i] %2 != 0) 
+                {
+                    create.Remove(create[i]);
+                }
+            }
+
+            NumberPrinter(create);
         }
 
-        private static void NumberChecker(List<int> numberList, int searchNumber)
+        private static void NumberChecker(List<int> create, int searchNumber)
         {
-            
+            if(create.Contains(searchNumber))
+            {
+                Console.WriteLine("We have that number!");
+            }
+            else
+            {
+                Console.WriteLine("That number is NOT here!");
+            }
         }
 
-        private static void Populater(List<int> numberList)
+        private static void Populater(List<int> create)
         {
+            
             Random rng = new Random();
+
+            while (create.Count <= 50)
+            {
+                create.Add(rng.Next(0, 51));
+            }
 
         }
 
@@ -127,11 +265,31 @@ namespace CollectionsMasterConsoleUI
         {
             Random rng = new Random();
 
+                        
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rng.Next(0, 51);
+
+            }
+            Console.WriteLine(numbers);
+
         }        
 
         private static void ReverseArray(int[] array)
         {
-            
+            var start = 0;
+            var end = array.Length - 1;
+
+            while (start < end)
+            {
+                int temp = array[start];
+                array[start] = array[end];
+                array[end] = temp;
+
+                start++;
+                end--;
+            }
+
         }
 
         /// <summary>
@@ -147,5 +305,9 @@ namespace CollectionsMasterConsoleUI
                 Console.WriteLine(item);
             }
         }
+
+        #endregion
+
     }
+
 }
---
+++ WindowsControlLibrary1/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WindowsControlLibrary1")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WindowsControlLibrary1")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("caaa27b5-46b2-461f-a0e1-d26276b93fd6")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
